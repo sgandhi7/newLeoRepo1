5468b737081635de6070b3bd09386c59b35ef900
Dalton Noftsger <dnoftsger@metrostar.com>
2024-02-14 14:49:36 -0500

    Fix auth

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,43 +1,11 @@
-import React, { useEffect } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { currentUser } from 'src/store';
+import React from 'react';
 
 export const Home = (): React.ReactElement => {
-  const [, setUser] = useRecoilState<string>(currentUser);
-
-  module.exports = async function (
-    context: { res: { body: { clientPrincipal: JSON } } },
-    req: { headers: { get: (arg0: string) => string } },
-  ) {
-    const header = req.headers.get('x-ms-client-principal');
-    const encoded = Buffer.from(header, 'base64');
-    const decoded = encoded.toString('ascii');
-
-    setUser(JSON.parse(decoded)['userDetails']);
-    context.res = {
-      body: {
-        clientPrincipal: JSON.parse(decoded),
-      },
-    };
-  };
-
-  async function getUser() {
-    const response = await fetch('/api/user');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  }
-
-  useEffect(() => {
-    getUser;
-    console.log(currentUser);
-  });
-
   return (
     <div className="grid-container">
       <div className="grid-row">
         <div className="grid-col">
-          <h1>Welcome {useRecoilValue(currentUser)}</h1>
+          <h1>Welcome</h1>
         </div>
       </div>
     </div>
