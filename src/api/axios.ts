3bf72101e141aa2da34403fa781d2b783e3b69a2
Dalton Noftsger <dnoftsger@metrostar.com>
2024-02-15 12:56:35 -0500

    Added key

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,10 +2,12 @@ import axios from 'axios';
 
 // Set up initial api instance
 const instance = axios.create({
+  withCredentials: true,
   baseURL: process.env.API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     // eslint-disable-next-line prettier/prettier
+    'x-functions-key': process.env.API_KEY
   },
 });
 
