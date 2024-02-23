1c450a05239253e49e9c22195a74ec96aa3dfe2d
Dalton Noftsger <dnoftsger@metrostar.com>
2024-02-23 10:08:05 -0500

    Fixed azure func

--- api/PromptFlowAPI/index.ts
+++ api/PromptFlowAPI/index.ts
@@ -1,6 +1,27 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
-import * as http from 'http';
-import * as https from 'https';
+import axios from 'axios';
+
+interface ChatHistoryItem {
+  inputs: Inputs;
+  outputs: Outputs;
+}
+
+interface Inputs {
+  query: string;
+}
+
+interface Outputs {
+  current_query_intent: string;
+  fetched_docs: string[];
+  output_entities: string;
+  reply: string;
+  search_intents: string;
+}
+
+interface Root {
+  query: string;
+  chat_history: ChatHistoryItem[];
+}
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -11,61 +32,46 @@ const httpTrigger: AzureFunction = async function (
   const apiKey: string = '5wKEL3TxvNP6UI54WQF4vND3LI7rY8Ct';
 
   if (!apiKey) {
-    context.res = {
-      status: 400,
-      body: 'A key should be provided to invoke the endpoint',
-    };
-    context.done();
-    return;
+    throw new Error('A key should be provided to invoke the endpoint');
   }
 
-  const headers: http.OutgoingHttpHeaders = {
+  const headers = {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + apiKey,
+    Authorization: `Bearer ${apiKey}`,
     'azureml-model-deployment': 'dvasquez-seattle-vcqoi-3',
   };
 
-  const bodyContent: string = req.body ? req.body.toString() : '';
-  const body: Buffer = Buffer.from(bodyContent, 'utf-8');
-
   try {
-    const options: https.RequestOptions = {
-      method: 'POST',
-      headers: headers,
+    const bodyContent = req.body;
+    const response = await axios.post(url, bodyContent, { headers });
+    const resultJson = response.data;
+
+    // Format response to match C# output structure
+    const rootObject: Root = {
+      query: bodyContent.query,
+      chat_history: [
+        {
+          inputs: { query: bodyContent.query },
+          outputs: {
+            current_query_intent: resultJson.current_query_intent,
+            fetched_docs: resultJson.fetched_docs,
+            output_entities: resultJson.output_entities,
+            reply: resultJson.reply,
+            search_intents: resultJson.search_intents,
+          },
+        },
+      ],
     };
 
-    const request = https.request(url, options, (res) => {
-      let responseBody: string = '';
-      res.on('data', (chunk) => {
-        responseBody += chunk;
-      });
-
-      res.on('end', () => {
-        const resultJson: Record<string, unknown> = JSON.parse(responseBody);
-        context.res = {
-          status: 200,
-          body: resultJson,
-        };
-        context.done();
-      });
-    });
-
-    request.on('error', (error) => {
-      context.res = {
-        status: 500,
-        body: error.message,
-      };
-      context.done();
-    });
-
-    request.write(body);
-    request.end();
+    context.res = {
+      status: 200,
+      body: rootObject,
+    };
   } catch (error) {
     context.res = {
       status: 500,
-      body: error.message,
+      body: error,
     };
-    context.done();
   }
 };
 
